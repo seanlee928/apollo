4f7e7ad91a723698b1f4a4001f39a546afb3af4e
Cao Long <dragon.caol@hd19-vm5.yunti.yh.aliyun.com>
2012-11-26 20:45:41 +0800

    add proto

--- hadoop.learn/src/main/java/com/apollo/mr/WordCount.java
+++ hadoop.learn/src/main/java/com/apollo/mr/WordCount.java
@@ -17,62 +17,66 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
-  public static final Log LOG = LogFactory.getLog(WordCount.class);
+	public static final Log LOG = LogFactory.getLog(WordCount.class);
 
-  public static class TokenizerMapper extends
-      Mapper<Object, Text, Text, IntWritable> {
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
-    public void map(Object key, Text value, Context context)
-        throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
-    }
-  }
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			Thread.sleep(10000);
+			System.out.println(key + ":" + value);
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
 
-  public static class IntSumReducer extends
-      Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
 
-    private IntWritable result = new IntWritable();
+		private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-        throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
-    }
-  }
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			Thread.sleep(10000);
+			System.out.println(key);
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    String[] otherArgs = new GenericOptionsParser(conf, args)
-        .getRemainingArgs();
-    if (otherArgs.length != 2) {
-      System.err.println("Usage: wordcount <in> <out>");
-      System.exit(2);
-    }
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
 
-    // org.apache.hadoop.io.LongWritable
-    Job job = new Job(conf, "word count");
-    job.setJarByClass(WordCount.class);
+		// org.apache.hadoop.io.LongWritable
+		Job job = new Job(conf, "dragon");
+		job.setJarByClass(WordCount.class);
 
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+//		job.setReducerClass(IntSumReducer.class);
 
-    job.setOutputKeyClass(Text.class);// add by dragon.caol
-    job.setOutputValueClass(IntWritable.class);// getMapOutputKeyClass
+		job.setOutputKeyClass(Text.class);// add by dragon.caol
+		job.setOutputValueClass(IntWritable.class);// getMapOutputKeyClass
 
-    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
